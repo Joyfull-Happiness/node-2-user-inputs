772e6152140ad2e9b3a1076981eeb68b787ccc20
Joyfull-Happiness <lajoie.bradley2@gmail.com>
2025-10-30 09:24:56 -0700

    updated comments

--- src/dogAgeCalculator.js
+++ src/dogAgeCalculator.js
@@ -10,23 +10,23 @@ node dogAgeCalculator.js
 */
 console.log(process.argv);
 
-// name
+// here i am establishing the dogName variable and assigning it to the third element in the array
 const dogName = process.argv[2];
 
-///age
+// here i am establishing the dogAge variable and assigning it to the fourth element in the array while using the built in node Number() function to convert the number to a string
+
 const dogAge = Number(process.argv[3]);
 
-//calculate dog age in human years if 1 Years old
+//declairing dog age in human years if 1 Years old
 let humanYearOne = 15;
 
-//calculate dog age in human years if 2 Years old
+//declairing dog age in human years if 2 Years old
 let humanYearTwo = 9;
 
-//calculate dog age in human years if 3 Years old or greater
+//declairing the dog age multiple for every other year after the second year to equal the number 5
 let everyOtherYear = 5;
 
 let humanYears;
-
 //if the dogAge is 1 then the humanYears will be the same as humanYearOne
 //if the dog age is not 1 then check if it is 2, if it is 2 then the human years will = humanyearone +humanyeartwo
 //if the dog age is not 2 then check if it is greater than = to 3 , if it is then the human years will = huanyearone +humanyeartwo + the dog's age -2 * every other year
